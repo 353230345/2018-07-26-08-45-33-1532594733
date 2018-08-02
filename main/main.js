1d0b8fe0f6ca668cb5b6d154fe6c899dcc4c3e53
sjq <353230345@qq.com>
2018-08-02 20:34:31 +0800

    res

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 // Write your cade below:
 module.exports = function main() {
-  
+  function result(a,b){
+	  var res;
+	  res=a%b;
+	  return res;
+  }
 };
+function result(9,4);
+function result(9,3);
